717a11358f47462c85d87d2f91aafcc07fe2d7ce
ajasseessi <ajasseessi@SUYPC214>
2017-09-07 17:40:02 +0530

    Add project files.

---
+++ MvcPartialviewPractice1/Controllers/EmployeesController.cs
@@ -0,0 +1,28 @@
+﻿using MvcPartialviewPractice1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Text;
+
+namespace MvcPartialviewPractice1.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        // GET: Employees
+        public ActionResult Index()
+        {
+            SchoolEntities db = new SchoolEntities();
+            return View(db.tbl_Employees);
+        }
+
+        public ActionResult IndexPart()//IndexPart is PartialView
+        {
+            SchoolEntities db = new SchoolEntities();
+            //return PartialView();//for partial and render partial.
+            return PartialView(db.tbl_Employees);//for render action.
+        }
+        
+    }
+}
